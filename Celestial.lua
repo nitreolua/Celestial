5a3546ab5ef93bb6724057d032b7a753c6f97050
nitreolua <inspirondelltss@gmail.com>
2025-01-25 23:27:15 +0200

    Add files via upload

--- Celestial.lua
+++ Celestial.lua
@@ -7,29 +7,5 @@ local Tab = Window:MakeTab({
     PremiumOnly = false
 })
 
---[[
-Name = <string> - The name of the tab.
-Icon = <string> - The icon of the tab.
-PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
-]]
 
-
-
-
-
-
-
-
-
-
---[[
-Name = <string> - The name of the UI.
-HidePremium = <bool> - Whether or not the user details shows Premium status or not.
-SaveConfig = <bool> - Toggles the config saving in the UI.
-ConfigFolder = <string> - The name of the folder where the configs are saved.
-IntroEnabled = <bool> - Whether or not to show the intro animation.
-IntroText = <string> - Text to show in the intro animation.
-IntroIcon = <string> - URL to the image you want to use in the intro animation.
-Icon = <string> - URL to the image you want displayed on the window.
-CloseCallback = <function> - Function to execute when the window is closed.
-]]
+OrionLib:Init()
